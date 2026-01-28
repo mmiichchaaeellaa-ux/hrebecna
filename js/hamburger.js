991edfe78540a39a95fa1243fdf024ad30c7a907
ZAT_LAN\m-vlckova <michaela.korimova@zat.cz>
2026-01-28 15:40:52 +0100

    úprava css a obsazenost na vánoce a silvestr

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
     const nav = document.getElementById("nav-menu");
+    const burger = document.getElementById("hamburger");
     nav.classList.toggle("open");
+    burger.classList.toggle("open");
 }
 
 document.querySelectorAll("#nav-menu a").forEach(link => {
